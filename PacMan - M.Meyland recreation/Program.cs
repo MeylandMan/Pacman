72a2548f852dee316cf4154176124a14e1b58cd2
M.Meyland <m.dunooman0@gmail.com>
2023-12-08 22:51:45 +0100

    added the code

--- PacMan - M.Meyland recreation/Program.cs
+++ PacMan - M.Meyland recreation/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using OpenTK.Platform;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        /*  Adding the window stuff with OpenGL   */
+        GameWindowSettings gws = GameWindowSettings.Default;
+        NativeWindowSettings nws = NativeWindowSettings.Default;
+        GameWindow window = new GameWindow(gws, nws);
+        window.Run();
+
+    }
+
+}
