ce7af7ffdc065a9ae7a51e5b133fc462f15c7a8d
MeylandMan <125752061+MeylandMan@users.noreply.github.com>
2023-12-19 10:02:26 +0100

    added files

---
+++ PacMan/PacMan/Game Settings.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Windowing.Desktop;
+using System;
+using OpenTK.Mathematics;
+using Basics;
+
+namespace PacMan_Game;
+
+internal class Game : GameWindow {
+    public Game() : base(GameWindowSettings.Default, NativeWindowSettings.Default) {
+        Console.WriteLine("Initializing the Game Window...");
+
+        //Centering this on monitor
+        this.CenterWindow(new Vector2i(Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT));
+        GameConsole.WriteLine("Resizing Window : " + Consts.SCREEN_WIDTH + ", " + Consts.SCREEN_HEIGHT);
+    }
+}
+
+internal class GameConsole {
+    public static void WriteLine(string message)  {
+        Console.WriteLine("-> " + message);
+    }
+}
---
+++ PacMan/PacMan/__init__.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Basics;
+
+internal class Consts {
+
+    public static int SCREEN_WIDTH = 640;
+    public static int SCREEN_HEIGHT = 480;
+}
