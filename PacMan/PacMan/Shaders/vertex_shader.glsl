4bc59abe42a25c62a9c7725c27bea45b6cf7feaf
MeylandMan <125752061+MeylandMan@users.noreply.github.com>
2023-12-26 12:20:28 +0100

    noone

---
+++ PacMan/PacMan/Shaders/vertex_shader.glsl
@@ -0,0 +1,13 @@
+﻿#Version 410
+
+layout(position=0) in vec3 vertices;
+layout(position=1) in vec3 colors;
+
+out vec3 fragColor;
+
+void main() {
+	fragColor = colors;
+
+	gl_position = vec4(vertices, 1.0);
+
+}
