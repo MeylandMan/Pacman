5cac7b2aaa7db18929c1a691342570a77d77097f
MeylandMan <125752061+MeylandMan@users.noreply.github.com>
2024-01-01 13:24:54 +0100

    Drawing squares

--- PacMan/PacMan/Main/Game Settings.cs
+++ PacMan/PacMan/Main/Game Settings.cs
@@ -1,9 +1,10 @@
-﻿using OpenTK.Mathematics;
+﻿using System;
+using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.Common;
 using System.Diagnostics;
-using StbImageSharp;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PacMan.Main;
 
@@ -13,29 +14,23 @@ internal class Game : GameWindow
     public double TargetUpdateFrequency = 30.0, TargetRenderFrequency = 30.0;
 
     float[] vertices = {
-
-        -0.5f, 0.5f, 0f, //Top left Vertex - 0 
-        0.5f, 0.5f, 0f, //Top right Vertex - 1
-        -0.5f, -0.5f, 0f, // Bottom left Vertex - 2
-        0.5f, -0.5f, 0f // Bottom right Vertex - 3
-
+        -0.5f, 0.5f, 0f, //Top Left Vertex - 0
+        0.5f, 0.5f, 0f, //Top Right Vertex - 1
+        -0.5f, -0.5f, 0f, // Bottom Left Vertex - 2
+        0.5f, -0.5f, 0f // Bottom Right Vertex - 3
     };
 
-    float[] texCoords = {
-        0f, 1f,
-        1f, 1f,
-        1f, 0f,
-        0f, 0f
-    };
-    uint[] indices = {
-        //Top triangle
-        0, 1, 3,
+    uint[] indices =
+    {
+        //First triangle
+        0, 1, 2,
 
-        //bottom triangle
-        2, 3, 0
+        // Second triangle
+        1, 2, 3
     };
+
     //Render Pipeline variables
-    int vao, vbo, shaderProgram, window_scale = 1, ebo, TextureID;
+    int vao, ebo, shaderProgram, window_scale = 1;
 
     int w, h;//width and height
     public Game() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
@@ -48,14 +43,14 @@ internal class Game : GameWindow
         CenterWindow(new Vector2i(w, h));
         GameConsole.WriteLine("Resizing Window : " + Consts.SCREEN_WIDTH + ", " + Consts.SCREEN_HEIGHT);
         //not allowing the player to cgange the size of the Window by their own
-        WindowBorder =  WindowBorder.Fixed;
+        WindowBorder = WindowBorder.Fixed;
 
         //The Aspect Ratio gonna always be 4:3
         AspectRatio = (4, 3);
 
         //Title of the Game
         Title = "PacMan - Recreation by M.Meyland";
-        
+
     }
 
     protected override void OnResize(ResizeEventArgs e)
@@ -73,43 +68,19 @@ internal class Game : GameWindow
 
         vao = GL.GenVertexArray();
 
-        // Bind the vao
-        GL.BindVertexArray(vao);
-
-        // --- vertices VBO ---
-        int TextureVBO, slot = 0;
-
-        vbo = GL.GenBuffer();
-        
+        int vbo = GL.GenBuffer(), slot = 0;
         GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
         GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-        GL.BindBuffer(BufferTarget.ArrayBuffer, slot);
-        // Put the vertex VBO in slot 0 of our VAO
 
+        // Bind the vao
+        GL.BindVertexArray(vao);
         GL.VertexAttribPointer(slot, 3, VertexAttribPointerType.Float, false, 0, 0);
         GL.EnableVertexAttribArray(slot);
-        // GL.EnableVertexArrayAttrib(vao, slot); WORKS FOR OPENGL 4.5 OR ABOVE
-
-        GL.BindBuffer(BufferTarget.ArrayBuffer, slot); //Unbinding the vbo
-
-        // --- Texture VBO ---
-
-        TextureVBO = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ArrayBuffer, TextureVBO);
-        GL.BufferData(BufferTarget.ArrayBuffer, texCoords.Length * sizeof(float), texCoords, BufferUsageHint.StaticDraw);
-
-        // Put the texture VBO in slot 1 of our VAO
-        GL.VertexAttribPointer(slot+1, 2, VertexAttribPointerType.Float, false, 0, 0);
-        GL.EnableVertexAttribArray(slot+1);
-
-        GL.BindBuffer(BufferTarget.ArrayBuffer, slot + 1); //Unbinding the texture
-        //GL.BindVertexArray(slot); // Unbinnding the vao
 
         ebo = GL.GenBuffer();
         GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
         GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
-
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, slot); // Unbinding the ebo
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
 
         //Create the shader program
         shaderProgram = GL.CreateProgram();
@@ -135,36 +106,13 @@ internal class Game : GameWindow
         GL.DeleteShader(VertexShader);
         GL.DeleteShader(FragmentShader);
 
-        // --- TEXTURES ----
-        TextureID = GL.GenTexture();
-
-        // Activate the texture in the unit
-        GL.ActiveTexture(TextureUnit.Texture0);
-        GL.BindTexture(TextureTarget.Texture2D, TextureID);
-
-        //Textures parameters
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
-
-        // Load Image
-        StbImage.stbi_set_flip_vertically_on_load(1);
-        ImageResult pacmanTex = ImageResult.FromStream(File.OpenRead("../../../Media/Textures/Pacman/walking/pacman_0.png"), ColorComponents.RedGreenBlueAlpha);
-
-        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, pacmanTex.Width, pacmanTex.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, pacmanTex.Data);
-
-        GL.BindTexture(TextureTarget.Texture2D, slot);
-
     }
     protected override void OnUnload()
     {
         // Always deleting the stuffs we don't need anymore
         GL.DeleteVertexArray(vao);
-        GL.DeleteBuffer(vbo);
-        GL.DeleteBuffer(ebo);
         GL.DeleteProgram(shaderProgram);
-        GL.DeleteTexture(TextureID);
+        GL.DeleteBuffer(ebo);
 
         Debug.Close();
         GameConsole.WriteLine("Debug ended.");
@@ -174,20 +122,16 @@ internal class Game : GameWindow
     protected override void OnRenderFrame(FrameEventArgs args)
     {
 
-        GL.ClearColor(0f, 0f, 0f, 1f);
+        GL.ClearColor(1f, 1f, 1f, 1f);
         GL.Clear(ClearBufferMask.ColorBufferBit);
 
 
-        //Draw our square
+        //Draw our triangle
         GL.UseProgram(shaderProgram);
-
-        GL.BindTexture(TextureTarget.Texture2D, TextureID);
-
 
         GL.BindVertexArray(vao);
         GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
         GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
-
         GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
         Context.SwapBuffers();
@@ -200,15 +144,20 @@ internal class Game : GameWindow
         base.OnUpdateFrame(args);
     }
 
-    public static string LoadShaderSource(string filePath) {
+    public static string LoadShaderSource(string filePath)
+    {
         string shaderSource = "";
 
-        try {
+        try
+        {
 
-            using(StreamReader reader = new StreamReader("../../../Shaders/" + filePath)) {
+            using (StreamReader reader = new StreamReader("../../../Shaders/" + filePath))
+            {
                 shaderSource = reader.ReadToEnd();
             }
-        } catch (Exception e){
+        }
+        catch (Exception e)
+        {
             Console.WriteLine("Failed to load shader source file : " + e.Message);
         }
 
--- PacMan/PacMan/obj/Debug/net8.0/PacMan.AssemblyInfo.cs
+++ PacMan/PacMan/obj/Debug/net8.0/PacMan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PacMan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac1a694808627315c93cd0aa79b5455fca9f001")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad168b5f2a1ece44ad963ea9890f28147d2881d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PacMan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PacMan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
