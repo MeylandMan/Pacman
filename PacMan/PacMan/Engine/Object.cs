dabaae9c3fc944712286c5544f26fbba47170d40
MeylandMan <125752061+MeylandMan@users.noreply.github.com>
2024-01-28 23:49:44 +0100

    Tried a lot for Object.cs (don't use object from C# casual ! )

---
+++ PacMan/PacMan/Engine/Object.cs
@@ -0,0 +1,82 @@
+﻿using OpenTK.Mathematics;
+using PacMan.Graphics;
+using PacMan.Main;
+using OpenTK.Graphics.OpenGL4;
+
+namespace PacMan.Engine;
+
+internal class Obj
+{
+    public Vector2 position;
+    public Vector2 velocity;
+    public Vector2 rotation;
+    public Vector2 scale;
+    public Mesh mesh;
+
+    public Obj(Vector2 Position, Vector2 Rotation, Vector2 Scale)
+    {
+        this.position = Position;
+        this.rotation = Rotation;
+        this.scale = Scale;
+    }
+
+    public static void LoadObjects(Mesh mesh, float[] vertices, uint[] indices, float[] texCoords, string textureFilePath) {
+        mesh = new(vertices, indices, texCoords, textureFilePath);
+    }
+    public static void DrawObjects(Mesh mesh, ShaderProgram program) { mesh.DrawMesh(program); }
+
+    public static void DeleteObjects(Mesh mesh){ mesh.DeleteMesh(); }
+}
+
+internal class Mesh {
+
+    // Graphics pipelines variables
+    private VAO vao;
+    private VBO textureVBO;
+    private IBO ibo;
+    private Texture texture;
+
+    public Mesh(float[] vertices, uint[] indices, float[] texCoords, string textureFilePath) {
+
+        vao = new();
+        VBO vbo = new(vertices);
+        vao.LinkToVAO(0, 3, vbo);
+
+        textureVBO = new(texCoords);
+        vao.LinkToVAO(1, 2, textureVBO);
+        textureVBO.UnBindVBO();
+
+        ibo = new(indices);
+        ibo.UnbindIBO();
+
+        texture = new(textureFilePath);
+    }
+
+    public void DrawMesh(ShaderProgram program)
+    {
+        vao.BindVAO();
+        ibo.BindIBO();
+        texture.BindTexture();
+
+        //Transformation matrices
+        Matrix4 model = Matrix4.Identity;
+        Matrix4 view = Matrix4.Identity;
+        Matrix4 projection = Matrix4.CreateOrthographic(Consts.WIDTH_ASPECT, 1f, 1f, -1f);
+
+        int modelLocation = GL.GetUniformLocation(program.ID, "model");
+        int viewLocation = GL.GetUniformLocation(program.ID, "view");
+        int projectionlLocation = GL.GetUniformLocation(program.ID, "projection");
+
+        GL.UniformMatrix4(modelLocation, true, ref model);
+        GL.UniformMatrix4(viewLocation, true, ref view);
+        GL.UniformMatrix4(projectionlLocation, true, ref projection);
+
+    }
+    public void DeleteMesh()
+    {
+
+        vao.DeleteVAO();
+        textureVBO.DeleteVBO();
+        ibo.DeleteIBO();
+    }
+}
--- PacMan/PacMan/Main/Game Settings.cs
+++ PacMan/PacMan/Main/Game Settings.cs
@@ -3,7 +3,8 @@ using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.Common;
 using System.Diagnostics;
-using StbImageSharp;
+using PacMan.Graphics;
+using PacMan.Engine;
 
 namespace PacMan.Main;
 
@@ -36,9 +37,15 @@ internal class Game : GameWindow
     };
 
     //Render Pipeline variables
-    int vao, ebo, TextureVBO, shaderProgram, window_scale = 1, textureID;
+    VAO vao;
+    VBO textureVBO;
 
-    int w, h; //width and height
+    IBO ibo;
+    Texture texture;
+    ShaderProgram program;
+
+
+    int w, h, window_scale = 1; //width and height
     public Game() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
     {
         Console.WriteLine("Initializing the Game Window...");
@@ -72,101 +79,34 @@ internal class Game : GameWindow
 
         base.OnLoad();
 
-        vao = GL.GenVertexArray();
+        /*
+        vao = new VAO();
+        VBO vbo = new VBO(vertices);
+        vao.LinkToVAO(0, 3, vbo);
 
-        // Bind the vao
-        GL.BindVertexArray(vao);
+        textureVBO = new VBO(texCoords);
+        vao.LinkToVAO(1, 2, textureVBO);
+        textureVBO.UnBindVBO();
 
-        // ---- Vertices VBO ----
-
-        int vbo = GL.GenBuffer(), slot = 0;
-        GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-        
-
-        // put the vertex vbo in slot 0 of our VAO
-        GL.VertexAttribPointer(slot, 3, VertexAttribPointerType.Float, false, 0, 0);
-        GL.EnableVertexAttribArray(slot);
-        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-
-        // ---- Texture VBO ----
-
-        TextureVBO = GL.GenBuffer();
-
-        GL.BindBuffer(BufferTarget.ArrayBuffer, TextureVBO);
-
-        GL.BufferData(BufferTarget.ArrayBuffer, texCoords.Length * sizeof(float), texCoords, BufferUsageHint.StaticDraw);
-        
-        // put the Texture in slot 1 of our VAO
-        GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 0, 0);
-        GL.EnableVertexAttribArray(1);
-
-        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-
-
-        ebo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
-        GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
-
-        //Create the shader program
-        shaderProgram = GL.CreateProgram();
-
-        //Vertex Shader setup
-        int VertexShader = GL.CreateShader(ShaderType.VertexShader);
-        GL.ShaderSource(VertexShader, LoadShaderSource("Default.vert"));
-        GL.CompileShader(VertexShader);
-
-        //Fragment shader setup
-        int FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
-        GL.ShaderSource(FragmentShader, LoadShaderSource("Default.frag"));
-        GL.CompileShader(FragmentShader);
-
-        //Attaching the vertex and fragment to the shaderProgram
-        GL.AttachShader(shaderProgram, VertexShader);
-        GL.AttachShader(shaderProgram, FragmentShader);
-
-        //Link them together
-        GL.LinkProgram(shaderProgram);
-
-        //Delete the shaders
-        GL.DeleteShader(VertexShader);
-        GL.DeleteShader(FragmentShader);
-
-        // --- TEXTURES ---
-        textureID = GL.GenTexture();
-
-        // Activate the texture in the unit
-        GL.ActiveTexture(TextureUnit.Texture0);
-        GL.BindTexture(TextureTarget.Texture2D, textureID);
-
-        // --- TEXTURES PARAMETERS
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (uint)TextureWrapMode.Repeat);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (uint)TextureWrapMode.Repeat);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (uint)TextureMinFilter.Nearest);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (uint)TextureMagFilter.Nearest);
-
-        // Load image
-        StbImage.stbi_set_flip_vertically_on_load(1);
-        ImageResult testTexture = ImageResult.FromStream(File.OpenRead("../../../Media/Textures/DirtTexture.jpg"), ColorComponents.RedGreenBlueAlpha);
-
-        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, testTexture.Width, testTexture.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, testTexture.Data);
-
-        //Unbind the texture
-        GL.BindTexture(TextureTarget.Texture2D, 0);
+        ibo = new IBO(indices);
+        ibo.UnbindIBO();
+        */
+        program = new ShaderProgram("Default.vert", "Default.frag");
 
     }
     protected override void OnUnload()
     {
         // Always deleting the stuffs we don't need anymore
-        GL.DeleteVertexArray(vao);
-        GL.DeleteProgram(shaderProgram);
-        GL.DeleteTexture(textureID);
-        GL.DeleteBuffer(ebo);
+        
+        vao.DeleteVAO();
+        program.DeleteProgram();
+        textureVBO.DeleteVBO();
+        ibo.DeleteIBO();
 
         Debug.Close();
         GameConsole.WriteLine("Debug ended.");
         Environment.Exit(0);
+
         base.OnUnload();
     }
     protected override void OnRenderFrame(FrameEventArgs args)
@@ -175,26 +115,26 @@ internal class Game : GameWindow
         GL.ClearColor(0.6f, 0.3f, 0.75f, 1f);
         GL.Clear(ClearBufferMask.ColorBufferBit);
 
-
+        /*
         //Draw our triangle
-        GL.UseProgram(shaderProgram);
-
-        GL.BindVertexArray(vao);
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
-        GL.BindTexture(TextureTarget.Texture2D, textureID);
+        program.BindProgram();
+        vao.BindVAO();
+        ibo.BindIBO();
+        texture.BindTexture();
 
         //Transformation matrices
         Matrix4 model = Matrix4.Identity;
         Matrix4 view = Matrix4.Identity;
         Matrix4 projection = Matrix4.CreateOrthographic(Consts.WIDTH_ASPECT, 1f, 1f, -1f);
 
-        int modelLocation = GL.GetUniformLocation(shaderProgram, "model");
-        int viewLocation = GL.GetUniformLocation(shaderProgram, "view");
-        int projectionlLocation = GL.GetUniformLocation(shaderProgram, "projection");
+        int modelLocation = GL.GetUniformLocation(program.ID, "model");
+        int viewLocation = GL.GetUniformLocation(program.ID, "view");
+        int projectionlLocation = GL.GetUniformLocation(program.ID, "projection");
 
         GL.UniformMatrix4(modelLocation, true, ref model);
         GL.UniformMatrix4(viewLocation, true, ref view);
         GL.UniformMatrix4(projectionlLocation, true, ref projection);
+        */
 
         GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
 
@@ -209,26 +149,6 @@ internal class Game : GameWindow
     {
 
         base.OnUpdateFrame(args);
-    }
-
-    public static string LoadShaderSource(string filePath)
-    {
-        string shaderSource = "";
-
-        try
-        {
-
-            using (StreamReader reader = new StreamReader("../../../Shaders/" + filePath))
-            {
-                shaderSource = reader.ReadToEnd();
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Failed to load shader source file : " + e.Message);
-        }
-
-        return shaderSource;
     }
 }
 
